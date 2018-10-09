5a38e1318cdd16229ba15ebce348061906ecc2ca
natalie246 <natalieakiva@gmail.com>
2018-10-09 21:03:06 +0300

    first commit push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+
+
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
+
+
+ constructor() {
+  super()
+
+  }
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?s=inception&apikey=ebfb52b7")
+      .then(res => res.json())
+      .then(
+        (result) => {
+
+          console.log(result)
+          // this.setState({
+          //   isLoaded: true,
+          //   items: result.items
+          // });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Hello world</h1>
+
+      
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
+import { createStore } from 'redux'
+// import rootReducer from './reducers'
+
+// import {store} from './store/index.js';
 import './index.css';
+
+// import App from './components/App'
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(rootReducer)
+
+ReactDOM.render(
+	// <Provider store={store}>
+
+	<App />
+	// </Provider>
+	, 
+	// document.getElementById('mount'));
+	document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
